a1bb647154b0f679d001c0bad2fa4ab6c029e0d5
eralio <ratsitoharaeralio@gmail.com>
2026-06-24 11:41:25 +0300

    css fait

--- pages/index.php
+++ pages/index.php
@@ -1,36 +1,48 @@
 <?php
-    include('../inc/functions.php');
-    $departments = get_all_departments();
+include('../inc/functions.php');
+$departments = get_all_departments();
 
-?>		
+?>
 <html>
-    <head>
-        <title>Les news</title>
-    </head>
-    <body>
-    <h1>Liste des départements</h1>
-    <p><a href="search.php">🔍 Rechercher un employé</a></p>
-    <p><a href="stats.php">📊 Statistiques par emploi</a></p>
-    <p><a href="dept_form.php">➕ Ajouter un département</a></p>
-    <p><a href="emp_form.php">➕ Ajouter un employé</a></p>
- <table border="1">
-    <tr>
-        <th>Department Number</th>
-        <th>Department Name</th>
-        <th>Manager actuel</th>
-        <th>Nombre d'employés</th>
-        <th>Action</th>
-    </tr>
-    <?php foreach ($departments as $line) {?>
-        <tr>
-            <td><a href="employees.php?dept_no=<?= urlencode($line['dept_no']) ?>"><?= $line['dept_no']?></a></td>
-            <td><?=$line['dept_name']?></td>
-            <td><?= $line['manager_name'] ?? '—' ?></td>
-            <td><?= $line['nb_employees'] ?></td>
-            <td><a href="dept_form.php?dept_no=<?= urlencode($line['dept_no']) ?>">Éditer</a></td>
-        </tr>
-    <?php } ?>
-    </table>
 
-    </body>
+<head>
+    <title>Les news</title>
+    <link rel="stylesheet" href="../design/theme-dark/style.css">
+</head>
+
+<body>
+    <div class="navbar">
+        <ul>
+            <li class="brand">
+                <h1>Liste des départements</h1>
+            </li>
+            <li><a href="search.php">🔍 Rechercher un employé</a></li>
+            <li><a href="stats.php">📊 Statistiques par emploi</a></li>
+            <li><a href="dept_form.php">➕ Ajouter un département</a></li>
+            <li><a href="emp_form.php">➕ Ajouter un employé</a></li>
+        </ul>
+    </div>
+
+    <div class="container">
+        <table border="1" class="table">
+            <tr>
+                <th>Department Number</th>
+                <th>Department Name</th>
+                <th>Manager actuel</th>
+                <th>Nombre d'employés</th>
+                <th>Action</th>
+            </tr>
+            <?php foreach ($departments as $line) { ?>
+                <tr>
+                    <td><a href="employees.php?dept_no=<?= urlencode($line['dept_no']) ?>"><?= $line['dept_no'] ?></a></td>
+                    <td><?= $line['dept_name'] ?></td>
+                    <td><?= $line['manager_name'] ?? '—' ?></td>
+                    <td><?= $line['nb_employees'] ?></td>
+                    <td><a href="dept_form.php?dept_no=<?= urlencode($line['dept_no']) ?>">Éditer</a></td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+</body>
+
 </html>
